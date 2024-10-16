cbee33c04e9f50af1383c987a244194f78ef2277
Sivaraj Vijithan <vijithan678@gmail.com>
2024-10-17 01:29:56 +0530

    CI CD workflow update

--- babel.config.ts
+++ babel.config.ts
@@ -1,4 +1,7 @@
-export const presets = [
-    ["@babel/preset-env", { targets: { node: 'current' } }],
-    "@babel/preset-react",
-];
+module.exports = {
+    presets: [
+        ["@babel/preset-env", { targets: { node: 'current' } }],
+        "@babel/preset-react",
+        "@babel/preset-typescript",
+    ],
+};
